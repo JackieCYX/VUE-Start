fe7207245484fd9feb957142681feb13aae29caf
kallin.chen <kallin.chen@globrobot.com>
2023-11-28 15:29:26 +0800

    第四天学习内容

---
+++ day04/03-父组件向子组件传值/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app" style="border: 3px solid #000; margin: 10px">
+    <Son :title="myTitle"></Son>
+  </div>
+</template>
+
+<script>
+import Son from './components/Son'
+export default {
+  name: 'App',
+  components: {
+    Son
+  },
+  data() {
+    return {
+      myTitle: 'TEST'
+    }
+  }
+}
+</script>
---
+++ day04/04-子组件向父组件传值/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app" style="border: 3px solid #000; margin: 10px">
+    我是APP组件
+    <!-- 2.父组件对子组件的消息进行监听 -->
+    <Son :title="myTitle" @changTitle="handleChange"></Son>
+  </div>
+</template>
+
+<script>
+import Son from './components/Son'
+export default {
+  name: 'App',
+  components: {
+    Son
+  },
+  data() {
+    return {
+      myTitle: 'FATHER TEST'
+    }
+  },
+  methods: {
+    handleChange(newValue) {
+      this.myTitle = newValue
+    }
+  }
+}
+</script>
