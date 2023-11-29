166afdd234da4c274005b3500952586931fddb8a
kallin.chen <kallin.chen@globrobot.com>
2023-11-29 12:35:34 +0800

    第五天学习内容提交

---
+++ day05/01-指令全局注册-局部注册/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>自定义指令</h1>
+    <input v-focus ref="inp" type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  // 注册指令
+  directives: {
+    // 指令名：指令的配置项
+    focus: {
+      inserted(el) {
+        el.focus()
+      }
+    }
+  }
+}
+</script>
