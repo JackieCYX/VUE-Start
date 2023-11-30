37f76a59af4ec018ba4562fa637f22b1302029d0
kallin.chen <kallin.chen@globrobot.com>
2023-11-30 13:00:58 +0800

    第六天学习内容提交

---
+++ day06/09-路由重定向/router/index.js
@@ -0,0 +1,16 @@
+import Home from '@/views/Home'
+import Search from '@/views/Search'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+// 创建一个路由对象
+const router = new VueRouter({
+    routes: [
+        { path: '/', redirect: '/home'},
+        { path: '/home', component: Home },
+        { path: '/search/:words?', component: Search }
+    ]
+})
+
+export default router
