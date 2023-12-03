dc6cf8db3ae51bb6c1d1f2873b64415b6f281944
JordanCYX <2338257659@qq.com>
2023-12-03 12:59:51 +0800

    第七天学习内容VUEX提交

---
+++ day07/vuex-demo/src/store/modules/setting.js
@@ -0,0 +1,21 @@
+// setting模块
+const state = {
+    theme: 'light',
+    desc: '测试demo'
+}
+const mutations = {
+    setTheme(state, newTheme) {
+        state.theme = newTheme
+    }
+}
+
+const actions = {}
+const getters = {}
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+    getters
+}
