8d4fd8333efc38c3022a7166049fd625d41b9ee0
kallin.chen <kallin.chen@globrobot.com>
2023-12-04 14:50:55 +0800

    实战项目结构初形成

---
+++ hm-shopping/src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import '@/utils/vant-ui'
+import '@/styles/common.less'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
