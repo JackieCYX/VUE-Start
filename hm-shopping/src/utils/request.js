936d8d651ccf5c64d564708b34e954af83fe97ff
kallin.chen <kallin.chen@globrobot.com>
2023-12-06 14:58:38 +0800

    登录页面完善，接口引入

--- hm-shopping/src/utils/request.js
+++ hm-shopping/src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Toast } from 'vant'
 
 const request = axios.create({
   baseURL: 'http://cba.itlike.com/public/index.php?s=/api/',
@@ -16,8 +17,13 @@ request.interceptors.request.use(function (config){
 
 // 添加响应拦截器
 request.interceptors.response.use(function (response) {
+  const res = response.data
+  if (res.status !== 200) {
+    Toast(res.message)
+    return Promise.reject(res.message)
+  }
   // 对响应数据做点什么
-  return response.data
+  return res
 }, function (error) {
   // 对响应错误做点什么
   return Promise.reject(error)
--- hm-shopping/src/views/login/index.vue
+++ hm-shopping/src/views/login/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <van-nav-bar title="会员登录" left-arrow @click-left="$router.go(-1)" />
+    <van-nav-bar title="会员登录" left-arrow @click-left="$router.go(-1)"/>
 
     <div class="container">
       <div class="title">
@@ -19,7 +19,7 @@
         <div class="form-item">
           <input v-model="msgCode" class="inp" placeholder="请输入短信验证码" type="text">
           <button @click="getCode">
-            {{ second === totalSecond ? '获取验证码' : second + '秒后重新发送'}}
+            {{ second === totalSecond ? '获取验证码' : second + '秒后重新发送' }}
           </button>
         </div>
       </div>
@@ -29,6 +29,92 @@
   </div>
 </template>
 
+<script>
+import {getPicCode, getMsgCode, codeLogin} from "@/api/login"
+
+export default {
+  async created() {
+    this.getPicCode()
+  },
+  data() {
+    return {
+      picUrl: '', // 存储请求渲染的图片地址
+      picKey: '', // 将来请求传递的图形验证码唯一标识
+      totalSecond: 60, // 总秒数
+      second: 60, // 倒计时的秒数
+      timer: null, // 定时器id
+      mobile: '', // 手机号
+      picCode: '', // 图形验证码
+    }
+  },
+  methods: {
+    // 获取图形验证码
+    async getPicCode() {
+      const {data: {base64, key}} = await this.getPicCode()
+      this.picUrl = base64
+      this.picKey = key
+    },
+    // 获取短信验证码
+    async getCode() {
+      if (!this.validFn()) {
+        // 如果没通过校验，没必要往下走了
+        return
+      }
+
+      // 当前目前没有定时器开着，且 totalSecond 和 second 一致 (秒数归位) 才可以倒计时
+      if (!this.timer && this.second === this.totalSecond) {
+        // 发送请求，获取验证码
+        await getMsgCode(this.picCode, this.picKey, this.mobile)
+        this.$toast('发送成功，请注意查收')
+
+        // 开启倒计时
+        this.timer = setInterval(() => {
+          this.second--
+
+          if (this.second < 1) {
+            clearInterval(this.timer)
+            this.timer = null
+            this.second = this.totalSecond
+          }
+        }, 1000)
+
+        // 发送请求，获取验证码
+        this.$toast('发送成功，请注意查收')
+      }
+    },
+    // 离开页面清除定时器
+    destroyed () {
+      clearInterval(this.timer)
+    },
+    // 校验 手机号 和 图形验证码 是否合法
+    // 通过校验，返回true
+    // 不通过校验，返回false
+    validFn () {
+      if (!/^1[3-9]\d{9}$/.test(this.mobile)) {
+        this.$toast('请输入正确的手机号')
+        return false
+      }
+      if (!/^\w{4}$/.test(this.picCode)) {
+        this.$toast('请输入正确的图形验证码')
+        return false
+      }
+      return true
+    },
+    async login () {
+      if (!this.validFn()) {
+        return
+      }
+      if (!/^\d{6}$/.test(this.msgCode)) {
+        this.$toast('请输入正确的手机验证码')
+        return
+      }
+      await codeLogin(this.mobile, this.msgCode)
+      this.$router.push('/')
+      this.$toast('登录成功')
+    }
+  }
+}
+</script>
 
 <style lang="less" scoped>
 .container {
@@ -36,10 +122,12 @@
 
   .title {
     margin-bottom: 20px;
+
     h3 {
       font-size: 26px;
       font-weight: normal;
     }
+
     p {
       line-height: 40px;
       font-size: 14px;
@@ -53,6 +141,7 @@
     margin-bottom: 14px;
     display: flex;
     align-items: center;
+
     .inp {
       display: block;
       border: none;
@@ -61,10 +150,12 @@
       font-size: 14px;
       flex: 1;
     }
+
     img {
       width: 94px;
       height: 31px;
     }
+
     button {
       height: 31px;
       border: none;
@@ -79,10 +170,10 @@
     width: 100%;
     height: 42px;
     margin-top: 39px;
-    background: linear-gradient(90deg,#ecb53c,#ff9211);
+    background: linear-gradient(90deg, #ecb53c, #ff9211);
     color: #fff;
     border-radius: 39px;
-    box-shadow: 0 10px 20px 0 rgba(0,0,0,.1);
+    box-shadow: 0 10px 20px 0 rgba(0, 0, 0, .1);
     letter-spacing: 2px;
     display: flex;
     justify-content: center;
